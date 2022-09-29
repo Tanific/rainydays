f29a51fb7ada676a5f99033d0cd118648e4f56dc
Tanific <stensentonje@gmail.com>
2022-09-29 14:48:40 +0200

    hamburger menu complete on home page

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-function menuButton () {
-    document.getElementById("myDropdown").classList.toggle("show");
+function menuButton() {
+  document.querySelector("#myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    const dropdown = document.querySelector(".dropdown-content");
+    const isShowing = dropdown.classList.contains("show");
+
+    if (isShowing) {
+      dropdown.classList.remove("show");
     }
-}
+  }
+};
