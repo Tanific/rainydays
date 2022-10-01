e21b5c77516c20d2f288c32fc5210f4d50dcfbfc
Tanific <stensentonje@gmail.com>
2022-10-02 00:01:36 +0200

    added counter to cart

---
+++ js/cart.js
@@ -0,0 +1,27 @@
+function countUp() {
+    const addOne = document.getElementById('value-number');
+    const newTotal = document.getElementById('subtotal-value');
+    
+    var i = addOne.value;
+    addOne.value = ++i;
+
+    var k = 150*addOne.value;
+    newTotal.value = k;
+}
+
+function countDown() {
+    const subtractOne = document.getElementById('value-number');
+    const newTotal = document.getElementById('subtotal-value');
+
+    var i = subtractOne.value;
+    if(i > 0)
+    {
+    subtractOne.value = --i;    
+    var k = newTotal.value-150;
+    newTotal.value = k;
+    }
+
+
+}
+
+
